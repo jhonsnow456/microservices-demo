f626c6898a4f3d578458c4558e159eb8eb5ae6c1
jhonsnow456 <thakuraman22july@gmail.com>
2023-05-07 16:13:00 +0530

    added a test for project sucessfully

---
+++ openapi/tests/index.test.js
@@ -0,0 +1,11 @@
+const fetch = require("node-fetch");
+const host = "http://localhost:8080";
+
+describe("get all catlogue data", () => {
+  it("should return 200 when data is in db", async () => {
+    const response = await fetch(host + "/catalogue");
+    const result = await response.json();
+
+    expect(response.status).toBe(200);
+  });
+});
